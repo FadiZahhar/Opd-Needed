be14ae3d674117513a8ad0a8bf4d02e25a0e4237
Fadi Nicolas Zahhar <79freelancer@gmail.com>
2024-01-13 19:54:52 +0200

    adding the meta data to be translated to arabic.

--- app/ar/page.tsx
+++ app/ar/page.tsx
@@ -1,4 +1,29 @@
 import FormAr from '@/components/formar'
+import { Metadata } from 'next'
+
+
+export const metadata: Metadata = {
+  title: 'OPD Needed',
+  description: 'تم تصميم هذا النموذج لجمع معلومات تفصيلية حول العقار المحدد الذي تحتاجه. من خلال تحديد احتياجاتك وتفضيلاتك، يمكننا مساعدتك بشكل أفضل في العثور على العقار المثالي الذي يلبي جميع معاييرك',
+  icons:'https://opd-needed.vercel.app/propertypro.svg',
+  openGraph: {
+    title: "OPD DOT VIP",
+    description:"التسويق العقاري الرقمي",
+    emails:'info@propertypro.vip',
+    phoneNumbers:'961 3 948 739',
+    siteName:"OPD DOT VIP",
+    images:'https://opd-needed.vercel.app/hero-img.jpg',
+    url:'https://propertypro.vip/',
+    countryName:'لبنان'
+},
+  twitter:{
+    site:'https://propertypro.vip/',
+    creator:"OPD DOT VIP",
+    description:'التسويق العقاري الرقمي',
+    title: "OPD DOT VIP",
+    images:'https://opd-needed.vercel.app/hero-img.jpg',
+  }
+}
 
 export default function Home() {
   return (
