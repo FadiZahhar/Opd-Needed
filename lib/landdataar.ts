2b1380aec2a230c14e4b30b29c30f2ebae5e18e6
Fadi Nicolas Zahhar <79freelancer@gmail.com>
2024-01-20 22:32:26 +0200

    finishing the translation land

--- lib/landdataar.ts
+++ lib/landdataar.ts
@@ -1,23 +1,23 @@
-export  const likeArray: string[] = ['','Buy','rent','Lease to buy']; 
+export  const likeArray: string[] = ['','شراء','إيجار','الإيجار للشراء']; 
 export  const specifyTypeArray: string[] = ['','أرض صالحة للبناء على أرض زراعية، أرض صناعية','أرض صالحة للبناء على أرض زراعية','أرض صناعية']; 
 export  const specifyRegionArray: string[] = ['','City','Town','Village'];
 export  const countryArray: string[] = ['','Lebanon','Turkey','Greece','USA']; 
 export const landClassification : string[] = ['','التزلج','A','B','B0','C','C1','C2','D','E','F'];
 export const moreDetails : string[] = ['','تصنيف سكني','التصنيف الزراعي','التصنيف الصناعي','التصنيف السياحي'];
-export const natureAndLocation : string[] = ['','flat or sloping',
-'rocky',
-'earthy',
-'mountain view',
-'sea view',
-'valley view'];
+export const natureAndLocation : string[] = ['','مسطحة ',
+'منحدرة',
+'صخرة',
+'ترابي',
+'مطلة على البحر',
+'مطلة على الوادي'];
 
-export const neighbourhood : string[] = ['','mosque',
-'church',
-'school',
-'shop/mall',
-'hospital/infirmary',
-'pharmacy',
-'high voltage electricity post/cable'];
+export const neighbourhood : string[] = ['','مسجد',
+'كنيسة',
+'مدرسة',
+'متجر / مول',
+'مستشفى / مستوصف',
+'صيدلية',
+'عمود/كابل كهرباء عالي الجهد'];
 
 export const isItPossibleTo : string[] = ['','drill an artesian','water well'];
 export const doesAnyoneHaveARight: string[] = ['','of way'];
